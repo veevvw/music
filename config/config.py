963ebf26fc9412c8f9a872e396f3499dd333481e
veevvw <149306702+veevvw@users.noreply.github.com>
2023-11-03 15:59:48 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/E9N99/musics",
+    "https://github.com/veevvw/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
